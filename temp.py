0f7c1dea1550289cef2f9280e7d373708987a3d0
MWold <m.fjeldwold@gmail.com>
2013-08-03 17:05:28 +0200

    First draft
    First version of temperature code, will be tested once wiring of circuit
    is complete. Only writes to standard output at the moment

--- temp.py
+++ temp.py
@@ -1,5 +1,39 @@
 import RP.GPIO as GPIO
-import Time
+import os
+import time
+import glob
+#Program to read the temperature from a digital sensor attached to the Pi
 
-#Program to read the temperature from a digital sensor attached to the raspberry pi
+
+#Shell commands to the OS to load drivers for sensor
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+#Find the file to which temperature is written
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0] #28 might need to be changed to appropriate serial
+device_file = device_folder + '/w1_slaver'
+
+#Get the raw data from the file
+def read_temp_raw():
+	f = open(device_file, 'r')
+	lines = f.readlines()
+	f.close
+	return lines
+	
+def read_temp():
+	lines = read_temp_raw()
+	while lines[0].strip[-3:] != 'YES':
+		time.sleep(0.2)
+		lines = read_temp_raw()
+	equals_pos = lines[1].find('t=')
+	if equals_pos != -1:
+		temp_string = lines[1][equals_pos+2:]
+		temp_c = float(temp_string) / 1000.0
+		return temp_c
+while True:
+	print(read_temp())
+	time.sleep(1)
+
+
 

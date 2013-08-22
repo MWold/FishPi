da28aa8b957c5534696e074909024501fab7cdc8
MWold <m.fjeldwold@gmail.com>
2013-08-22 13:20:53 +0000

    Fixed some typing errors, program now continously reads temperature from sensor and writes it to standard output

--- temp.py
+++ temp.py
@@ -1,29 +1,28 @@
-import RP.GPIO as GPIO
 import os
 import time
 import glob
 #Program to read the temperature from a digital sensor attached to the Pi
 
-
 #Shell commands to the OS to load drivers for sensor
+os.system('cd /')
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 #Find the file to which temperature is written
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0] #28 might need to be changed to appropriate serial
-device_file = device_folder + '/w1_slaver'
+device_folder = glob.glob(base_dir + '28*')[0] 
+device_file = device_folder + '/w1_slave'
 
 #Get the raw data from the file
 def read_temp_raw():
 	f = open(device_file, 'r')
 	lines = f.readlines()
-	f.close
+	f.close()
 	return lines
 	
 def read_temp():
 	lines = read_temp_raw()
-	while lines[0].strip[-3:] != 'YES':
+	while lines[0].strip()[-3:] != 'YES':
 		time.sleep(0.2)
 		lines = read_temp_raw()
 	equals_pos = lines[1].find('t=')
@@ -34,6 +33,3 @@ def read_temp():
 while True:
 	print(read_temp())
 	time.sleep(1)
-
-
-
